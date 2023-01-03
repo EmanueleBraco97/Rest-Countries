2ca15a8b732d6dee6914f1ee435e366e4822a90e
Emanuele Braco <emanuele.braco@gmail.com>
2023-01-03 11:33:00 +0100

    bugfix media query and navbar css

--- rest-countries/src/components/Header/Navbar.js
+++ rest-countries/src/components/Header/Navbar.js
@@ -13,7 +13,9 @@ const Navbar = () => {
   return (
     <nav className={darkMode ? `nav-dark` : `nav-light`} id="nav">
       <div className="nav-left">
-        <h3>Where in the world?</h3>
+        <h3 className={darkMode ? `h3-dark` : `h3-light`}>
+          Where in the world?
+        </h3>
       </div>
 
       <div className="nav-right">
